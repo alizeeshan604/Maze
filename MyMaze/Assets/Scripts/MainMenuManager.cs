07e20cb7469614fdce500b85166e0e7e9a2e811c
Hassan Hadayat <hassanhadayat3@gmail.com>
2022-07-20 17:20:52 +0500

    main menu screen

---
+++ MyMaze/Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public GameObject mainMenuPanel;
+    public GameObject aboutUsPanel;
+
+    private void OnEnable()
+    {
+        // Load Main Menu Panel
+        aboutUsPanel.SetActive(false);
+        mainMenuPanel.SetActive(true);
+    }
+    public void OnClick_PlayBtn()
+    {
+        // Load Level Selection Scene
+        SceneManager.LoadScene(1);
+    }
+
+    public void OnClick_AboutUsBtn()
+    {
+        // Load About Us Panel
+        mainMenuPanel.SetActive(false);
+        aboutUsPanel.SetActive(true);
+
+    }
+}
