ab567d05550aca5f3f2f0290cf488885e9d4ab1b
Sohaib Ali Khan <84563883+sohaib1268@users.noreply.github.com>
2022-07-20 17:24:02 +0500

    bata complete till level1

--- MyMaze/Assets/Scripts/LevelChange.cs
+++ MyMaze/Assets/Scripts/LevelChange.cs
@@ -2,46 +2,51 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 using TMPro;
 
 public class LevelChange : MonoBehaviour
 {
     int level = 1;
     public TextMeshProUGUI mytext;
-    //public Sprite[] image;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Sprite[] imageDisplay;
+    public Image levelImage;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
     public void changelevelfwd()
     {
-        if (level < 7)
+        if (level < 6)
         {
             level += 1;
-            mytext.text = "MAP " + level.ToString();
-        }
-        else
-        {
-            mytext.text = "MAP " + level.ToString();
+            mytext.text = "MAP " + (level).ToString();
+            ActivateImage(level);
+
         }
     }
+    
     public void changelevelbcwd()
     {
         if (level > 1)
         {
             level -= 1;
-            mytext.text = "MAP " + level.ToString();
-        }
-        else
-        {
-            mytext.text = "MAP " + level.ToString();
+            mytext.text = "MAP " + (level).ToString();
+            ActivateImage(level);
         }
     }
+
+    public void ActivateImage(int index)
+    {
+        levelImage.sprite = imageDisplay[index-1];
+    }
+
+
+    public void loadlevel()
+    {
+        print(level);
+        if (level == 1)
+        {
+            SceneManager.LoadScene("MapOne");
+        }
+    }
+
 }
